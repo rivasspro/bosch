974ce897f88666e1f1c4b8ac872ca06b997511d5
David Rivas @MBP <david22rivas@gmail.com>
2023-04-20 04:22:01 -0600

    Category of products Category page List Page ?icons

---
+++ list-page.php
@@ -0,0 +1,75 @@
+<?php
+include('encabezados.php');
+?>
+
+<section class="categoryCopy section">
+  <div class="contenedor">
+    <div class="category__copy">
+      <h1>List Page</h1>
+      <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Vitae distinctio quas omnis dolorum cum ullam!</p>
+    </div>
+  </div>
+</section>
+<section class="categoryPage">
+  <div class="categoryPage__item new">
+    <div class="contenedor-fluid">
+      <div class="categoryPage__wrapper contenedor">
+        <div class="categoryPage__flag">
+          <div class="categoryPage__flag-item">
+            new
+          </div>
+        </div>
+        <div class="categoryPage__start">
+          <p>Lorem ipsum dolor sit.</p>
+          <a class="title" href="#">Lorem, ipsum dolor.</a>
+        </div>
+        <div class="categoryPage__center">
+          <p>Lorem, ipsum dolor sit amet consectetur adipisicing </p>
+          <ul>
+            <li>Lorem ipsum dolor sit amet, consectetur adipisicing elit.</li>
+            <li>Lorem ipsum dolor sit amet, consectetur adipisicing elit.</li>
+            <li>Lorem ipsum dolor sit amet, consectetur adipisicing elit.</li>
+            <li>Lorem ipsum dolor sit amet, consectetur adipisicing elit.</li>
+          </ul>
+          <div class="btn btn-primary">Lorem, ipsum.</div>
+        </div>
+        <div class="categoryPage__end">
+          <img src="./img/list_page.png" alt="">
+        </div>
+      </div>
+    </div>
+  </div>
+  <div class="categoryPage__item all">
+    <div class="contenedor-fluid">
+      <div class="categoryPage__wrapper contenedor">
+      <div class="categoryPage__flag">
+          <div class="categoryPage__flag-item">
+            new
+          </div>
+        </div>
+        <div class="categoryPage__start">
+          <p>Lorem ipsum dolor sit amet.</p>
+          <a class="title" href="#">Lorem, ipsum dolor.</a>
+        </div>
+        <div class="categoryPage__center">
+          <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit.</p>
+          <ul>
+            <li>Lorem ipsum dolor sit amet, consectetur adipisicing elit.</li>
+            <li>Lorem ipsum dolor sit amet, consectetur adipisicing elit.</li>
+            <li>Lorem ipsum dolor sit amet, consectetur adipisicing elit.</li>
+            <li>Lorem ipsum dolor sit amet, consectetur adipisicing elit.</li>
+          </ul>
+          <div class="btn btn-primary">Lorem, ipsum.</div>
+        </div>
+        <div class="categoryPage__end">
+          <img src="./img/list_page.png" alt="">
+        </div>
+      </div>
+    </div>
+  </div>
+</section>
+
+
+<?php
+include('footer.php');
+?>
