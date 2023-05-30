442dd47f4a6426b9a472efd0b087e467e07c58b5
David Rivas @MBP <david22rivas@gmail.com>
2023-05-30 14:17:20 -0600

    contactPage 1.0

---
+++ contact.php
@@ -0,0 +1,37 @@
+<?php
+include('encabezados.php');
+include('funciones.php');
+?>
+<section class="contactPage">
+  <div class="contactPage__hero"></div>
+  <div class="contactPage__text contenedor">
+    <div class="contactPage__info">
+      <h1>Lorem ipsum dolor sit amet.</h1>
+      <h5>Lorem ipsum dolor </h5>
+    </div>
+  </div>
+</section>
+<section class="contactPage_form">
+  <div class="contenedor">
+
+    <form action="">
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsa.
+      </p>
+      <div class="formField select">
+        <label for="">Lorem</label>
+        <select name="" id="">
+          <option value="">Lorem 1</option>
+          <option value="">Torem 2</option>
+        </select>
+      </div>
+      <div class="formField text">
+        <label for="">Lorem ipsum</label>
+        <input type="text">
+      </div>
+
+    </form>
+  </div>
+</section>
+<?php
+include('footer.php');
+?>
--- encabezados.php
+++ encabezados.php
@@ -10,7 +10,7 @@
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/swiper@9/swiper-bundle.min.css" />
   <link rel="stylesheet" href="./css/bosch.min.css?v=<?= rand(1001, 2000) ?>">
-  <title>Document</title>
+  <title>Clone 🐶🦊</title>
 </head>
 
 <body>
