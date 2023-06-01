0392b1ebb00ff4f078618a886b8f59e0cf5e74fe
David Rivas @c3ntro <david.rivas@c3ntro.com>
2023-06-01 12:42:13 -0600

    sectionsPage 1.0

---
+++ sections.php
@@ -0,0 +1,51 @@
+<?php
+include('encabezados.php');
+?>
+<section class="contactPage">
+  <div class="contactPage__hero --sections"></div>
+  <div class="contactPage__text contenedor">
+    <div class="contactPage__info">
+      <h1 class="--sections">Lorem ipsum dolor</h1>
+    </div>
+  </div>
+</section>
+
+<section class="sectionsPage">
+  <div class="contenedor">
+    <h2>LOREM - Ipsum dolor sit amet consectetur.</h2>
+    <div class="sectionsPage__item">
+      <img src="./img/sections_hero.jpg" alt="">
+      <div>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Qui molestias porro minima similique ipsa, illo modi laudantium dolor blanditiis ab neque ea consectetur.</p>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Qui molestias porro minima similique ipsa, illo modi laudantium dolor blanditiis ab neque ea consectetur.</p>
+        <a href="#" class="Link"><span class="Link_label">lorem ipsum</span></a>
+      </div>
+    </div>
+  </div>
+</section>
+
+<section class="sectionsPageAll">
+  <div class="contenedor">
+    <div class="sectionsPageAll__item">
+      <img src="./img/sections_hero.jpg" alt="">
+      <div>
+        <h2>Lorem, ipsum dolor.</h2>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Qui molestias porro minima similique ipsa, illo modi laudantium dolor blanditiis ab neque ea consectetur.</p>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Qui molestias porro minima similique ipsa, illo modi laudantium dolor blanditiis ab neque ea consectetur.</p>
+      </div>
+    </div>
+    <div class="sectionsPageAll__item">
+      <img src="./img/sections_hero.jpg" alt="">
+      <div>
+        <h2>Lorem, ipsum dolor.</h2>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Qui molestias porro minima similique ipsa, illo modi laudantium dolor blanditiis ab neque ea consectetur.</p>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Qui molestias porro minima similique ipsa, illo modi laudantium dolor blanditiis ab neque ea consectetur.</p>
+      </div>
+    </div>
+  </div>
+</section>
+
+
+<?php
+include('footer.php');
+?>
