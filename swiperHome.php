894793cf327659703692a3956d0489df1226478c
David Rivas @c3ntro <david.rivas@c3ntro.com>
2023-06-01 19:51:50 -0600

    homePage 26%

--- swiperHome.php
+++ swiperHome.php
@@ -5,21 +5,24 @@
       <div class="swiper-content">
         <div class="contenedor">
           <div class="swiper-text">
-            <h2 class="h1">Lorem ipsum dolor sit amet consectetur adipisicing iure!</h2>
+            <h2 class="h1">1 Lorem ipsum dolor sit amet consectetur adipisicing iure!</h2>
             <a href="#" class="Link"><div class="Link_label">Lorem ipsum dolor sit.</div></a>
           </div>
         </div>
       </div>
     </div>
     <div class="swiper-slide">
-      <img src="img/home-page/0.png" alt="">
+      <div class="swiper-media"><img src="img/home-page/0.png" alt=""></div>
+      <div class="swiper-content">
+        <div class="contenedor">
+          <div class="swiper-text">
+            <h2 class="h1">2 Lorem ipsum dolor sit amet consectetur adipisicing iure!</h2>
+            <a href="#" class="Link"><div class="Link_label">Lorem ipsum dolor sit.</div></a>
+          </div>
+        </div>
+      </div>
     </div>
-    <div class="swiper-slide">
-      <img src="img/home-page/0.png" alt="">
-    </div>
-    <div class="swiper-slide">
-      <img src="img/home-page/0.png" alt="">
-    </div>
+    
   </div>
 
   <div class="swiper-button-next"></div>
