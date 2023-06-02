194e8f85dccc24f4a10d47584d27b93d82e7d35a
David Rivas @c3ntro <david.rivas@c3ntro.com>
2023-06-02 15:05:03 -0600

    homePage 95%

--- index.php
+++ index.php
@@ -12,16 +12,66 @@ include('encabezados.php');
     <h2 class="h1">Lorem ipsum dolor sit amet.</h2>
     <div class="homePage__s2_wrapper">
       <div class="homePage__s2_wrapper_item">
-        <img src="" alt="">
-        <h3>Lorem, ipsum dolor.</h3>
-        <a href="#" class="Btn">
-          <span class="Btn_label">lorem</span>
-        </a>
+        <div class="homePage__s2_wrapper_item_img">
+          <img src="./img/home-page/h0.jpg" alt="">
+        </div>
+        <div>
+          <h5>Lorem, ipsum dolor.</h5>
+          <a href="#" class="Link">
+            <span class="Link_label">lorem</span>
+          </a>
+        </div>
+      </div>
+      <div class="homePage__s2_wrapper_item">
+        <div class="homePage__s2_wrapper_item_img">
+          <img src="./img/home-page/h0.jpg" alt="">
+        </div>
+        <div>
+          <h5>Lorem, ipsum dolor.</h5>
+          <a href="#" class="Link">
+            <span class="Link_label">lorem</span>
+          </a>
+        </div>
+      </div>
+      <div class="homePage__s2_wrapper_item">
+        <div class="homePage__s2_wrapper_item_img">
+          <img src="./img/home-page/h0.jpg" alt="">
+        </div>
+        <div>
+          <h5>Lorem, ipsum dolor.</h5>
+          <a href="#" class="Link">
+            <span class="Link_label">lorem</span>
+          </a>
+        </div>
       </div>
     </div>
   </div>
 </section>
-
+<section class="homePage__s3">
+  <div class="contenedor">
+    <div class="homeProducts">
+      <div class="homeProducts__title">
+        <h2>Lorem, ipsum dolor.</h2>
+      </div>
+      <div class="homeProducts__list">
+        <div class="homeProducts__list_item">
+          <a href="#">
+            <img src="./img/home-page/p0.png" alt="">
+            <span>Lorem, ipsum.</span>
+          </a>
+        </div>
+        <!--  -->
+        <div class="homeProducts__list_item">
+          <a href="#">
+            <img src="./img/home-page/p0.png" alt="">
+            <span>Lorem, ipsum.</span>
+          </a>
+        </div>
+        <!--  -->
+      </div>
+    </div>
+  </div>
+</section>
 
 <?php
 include('atc_form.php');
