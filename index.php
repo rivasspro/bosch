a88e62f8cc52547c30dcf1361ded9bec9a0c46d2
David Rivas @c3ntro <david.rivas@c3ntro.com>
2023-06-05 17:46:03 -0600

    homePage 100%

--- index.php
+++ index.php
@@ -60,14 +60,140 @@ include('encabezados.php');
             <span>Lorem, ipsum.</span>
           </a>
         </div>
-        <!--  -->
         <div class="homeProducts__list_item">
           <a href="#">
             <img src="./img/home-page/p0.png" alt="">
             <span>Lorem, ipsum.</span>
           </a>
         </div>
-        <!--  -->
+        <div class="homeProducts__list_item">
+          <a href="#">
+            <img src="./img/home-page/p0.png" alt="">
+            <span>Lorem, ipsum.</span>
+          </a>
+        </div>
+        <div class="homeProducts__list_item">
+          <a href="#">
+            <img src="./img/home-page/p0.png" alt="">
+            <span>Lorem, ipsum.</span>
+          </a>
+        </div>
+        <div class="homeProducts__list_item">
+          <a href="#">
+            <img src="./img/home-page/p0.png" alt="">
+            <span>Lorem, ipsum.</span>
+          </a>
+        </div>
+        <div class="homeProducts__list_item">
+          <a href="#">
+            <img src="./img/home-page/p0.png" alt="">
+            <span>Lorem, ipsum.</span>
+          </a>
+        </div>
+        <div class="homeProducts__list_item">
+          <a href="#">
+            <img src="./img/home-page/p0.png" alt="">
+            <span>Lorem, ipsum.</span>
+          </a>
+        </div>
+        <div class="homeProducts__list_item">
+          <a href="#">
+            <img src="./img/home-page/p0.png" alt="">
+            <span>Lorem, ipsum.</span>
+          </a>
+        </div>
+      </div>
+    </div>
+  </div>
+</section>
+<section class="homePage__s4">
+  <div class="contenedor">
+    <div class="grid__3">
+      <div class="grid__3_title">
+        <h2>Lorem ipsum dolor sit amet.</h2>
+      </div>
+      <div class="grid__3_item">
+        <a href="#">
+          <img src="./img/home-page/hp0.webp" alt="">
+          <div class="grid__3_item-text">
+            <h6>Lorem, ipsum.</h6>
+            <h3>Lorem ipsum, dolor sit amet co</h3>
+          </div>
+        </a>
+      </div>
+      <div class="grid__3_item">
+        <a href="#">
+          <img src="./img/home-page/hp0.webp" alt="">
+          <div class="grid__3_item-text">
+            <h6>Lorem, ipsum.</h6>
+            <h3>Lorem ipsum, dolor sit amet co</h3>
+          </div>
+        </a>
+      </div>
+      <div class="grid__3_item">
+        <a href="#">
+          <img src="./img/home-page/hp0.webp" alt="">
+          <div class="grid__3_item-text">
+            <h6>Lorem, ipsum.</h6>
+            <h3>Lorem ipsum, dolor sit amet co</h3>
+          </div>
+        </a>
+      </div>
+      <div class="grid__3_item">
+        <a href="#">
+          <img src="./img/home-page/hp0.webp" alt="">
+          <div class="grid__3_item-text">
+            <h6>Lorem, ipsum.</h6>
+            <h3>Lorem ipsum, dolor sit amet co</h3>
+          </div>
+        </a>
+      </div>
+      <div class="grid__3_item">
+        <a href="#">
+          <img src="./img/home-page/hp0.webp" alt="">
+          <div class="grid__3_item-text">
+            <h6>Lorem, ipsum.</h6>
+            <h3>Lorem ipsum, dolor sit amet co</h3>
+          </div>
+        </a>
+      </div>
+
+    </div>
+  </div>
+</section>
+<section class="homePage__s5">
+  <div class="contenedor">
+    <div class="grid__3">
+      <div class="grid__3_title">
+        <h2>Lorem ipsum dolor sit amet.</h2>
+      </div>
+      <div class="grid__3_item">
+        <img src="./img/home-page/hp0.webp" alt="">
+        <div class="grid__3_item-text">
+          <h3>Lorem ipsum, dolor sit amet co</h3>
+          <a href="#" class="Link"><span class="Link_label">Lorem, ipsum.</span></a>
+        </div>
+      </div>
+      <div class="grid__3_item">
+        <img src="./img/home-page/hp0.webp" alt="">
+        <div class="grid__3_item-text">
+          <h3>Lorem ipsum, dolor sit amet consectt. Quaerat!</h3>
+          <a href="#" class="Link"><span class="Link_label">Lorem, ipsum.</span></a>
+        </div>
+      </div>
+      <div class="grid__3_item">
+        <img src="./img/home-page/hp0.webp" alt="">
+        <div class="grid__3_item-text">
+          <h3>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quaerat!</h3>
+          <a href="#" class="Link"><span class="Link_label">Lorem, ipsum.</span></a>
+        </div>
+      </div>
+      <div class="grid__3_item">
+        <img src="./img/home-page/hp0.webp" alt="">
+        <div class="grid__3_item-text">
+          <h3>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quaerat!</h3>
+          <a href="#" class="Link"><span class="Link_label">Lorem, ipsum.</span></a>
+        </div>
       </div>
     </div>
   </div>
